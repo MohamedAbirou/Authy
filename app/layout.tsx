8e0306b2d96f72922b8e4d831805483d079e5eef
Mohamed Abirou <abiroumohamed58@gmail.com>
2024-01-05 18:12:29 +0100

    chore: Website description updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Authy",
-  description: "An advanced authentication service platform",
+  description: "An authentication service platform",
 };
 
 export default function RootLayout({
