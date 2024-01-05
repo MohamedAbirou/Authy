d05e4964e1984c60889fced43742919414bfda4a
Mohamed Abirou <abiroumohamed58@gmail.com>
2024-01-05 19:00:17 +0100

    feat: set token expiration duration to 15 mins

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -32,6 +32,7 @@ export const generateVerificationToken = async (email: string) => {
 
 export const generatePasswordResetToken = async (email: string) => {
   const token = uuidv4();
+  // Password reset token expires after 15 minutes
   const expires = new Date(new Date().getTime() + 3600 * 1000);
 
   const existingToken = await getPasswordResetTokenByEmail(email);
